67e136dee35237bc1ca8b0464fb4dafa32c8c315
Yoshihiro UEDA <ueda@do-luck.net>
2016-04-01 20:34:00 +0900

    PDFファイルのダウンロード部分の作成完了

--- IPA/src/TMain.java
+++ IPA/src/TMain.java
@@ -1,17 +1,26 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class TMain {
-
+	static final String  urlmoji = "https://www.jitec.ipa.go.jp/1_04hanni_sukiru/mondai_kaitou_2015h27_2/2015h27a_fe_am_qs.pdf";
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
-		new TDownload( "http://www.yahoo.co.jp", "c:\"","saas.txt");
+		String [] list = urlmoji.split("/");
+		String filename = list[list.length-1];
+		
+		new TDownload( urlmoji,
+				"C:\\Users\\ueda\\Desktop",filename);
+		
 
 	}
 
@@ -20,27 +29,39 @@ public class TMain {
 class TDownload {
 
 	public TDownload(String urlString, String saveFolderName, String saveFileName) {
+		
+		System.out.println( "urlString = " + urlString);
+		System.out.println( "saveFolderName = " + saveFolderName);
+		System.out.println( "saveFilename = " + saveFileName	);
+		String saveFilePath = saveFolderName+File.separator+saveFileName; 
+				System.out.println( "savePath = "+saveFilePath);
+		
+		
 		URL url = null;
-		HttpURLConnection urlcon = null;
+		URLConnection urlcon=null;
+		
 		BufferedReader br =null; 
 		FileOutputStream fos =null;
 		
 		try {
 			url = new URL(urlString);
-			urlcon = (HttpURLConnection) url.openConnection();
-			urlcon.setRequestMethod("GET");
+			 urlcon = url.openConnection();
+			InputStream is = urlcon.getInputStream();
+			File fout = new File( saveFilePath	);
+			FileOutputStream out = new FileOutputStream( fout, false	);
+			byte [] bytes = new byte[1024*1024	];
 			
-			br=new BufferedReader( new InputStreamReader( urlcon.getInputStream()));
-			String line ;
-			
-			System.out.println( "BufferedReader Ok.");
-			while ( null != ( line=br.readLine())){
-				System.out.println(line);
+			while(true ){
+				int ret = is.read(bytes);
+//				System.out.print( "["+ ret+"]");
+				if ( ret <=0 )			break;
+				out.write(bytes,0,ret);
 				
 			}
-			br.close();
-			br=null;
-			
+			out.close();
+			out=null;
+			is.close();
+			is=null;
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			System.out.println("URL Object cannot created.");
